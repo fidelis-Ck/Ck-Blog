1ba68c38484f42a6fc5dca72e4534cf4495d48a2
fidelis-Ck <62496301+fidelis-Ck@users.noreply.github.com>
2020-03-22 16:34:09 +0530

    Trial Changes

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,12 +1,12 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
+    title: `Ck  Blog`,
     author: {
-      name: `Kyle Mathews`,
+      name: `Fidelis`,
       summary: `who lives and works in San Francisco building useful things.`,
     },
     description: `A starter blog demonstrating what Gatsby can do.`,
-    siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
+    siteUrl: `https://ck-blog.netlify.com/`,
     social: {
       twitter: `kylemathews`,
     },
